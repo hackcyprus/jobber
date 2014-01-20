a45d1869f64a50f463c4ddbc90103591abe705fd
Alex Michael <hi@alexmic.net>
2014-01-20 23:45:25 +0100

    Ensure placeholders are everywhere and with the right color

--- jobber/static/js/search.js
+++ jobber/static/js/search.js
@@ -1,6 +1,9 @@
 (function($, global) {
 
     var $search = $('#search');
+
+    $search.placeholder();
+
     $search.keydown(function(e) {
         if (e.which != 13) return;
         window.location = '/search/' + $search.val();
